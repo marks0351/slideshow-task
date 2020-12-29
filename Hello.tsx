cd51c3ca24807186a7340ef90f6c158319bbb251
Ashok Kumar M <marks.s.t.johns@gmail.com>
2020-12-29 10:53:44 +0530

    adding slideshow with new react app

--- Hello.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { Slider } from "./Slider";
-
-export default ({ name }) => <Slider />;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Slider } from './Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
